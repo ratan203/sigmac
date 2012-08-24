dc6cf00aefa7c490282d2052cf2fa816e298bfd6
ekarannagoda <ekarannagoda@gmail.com>
2012-08-24 02:55:27 +0000

    Adding SigmaC Database Server

---
+++  sigmac/SCDBServer/src/server/DBManager.java
@@ -0,0 +1,167 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package server;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import smc.Concept;
+import smc.Document;
+import smc.RelatedConcept;
+/**
+ *
+ * @author Thilina
+ */
+public class DBManager {
+	
+public void updateDB(Connection con, Document doc){
+        HashMap<String,Concept> concept=doc.getDoc();
+        HashMap<String,ArrayList<RelatedConcept>> relationships=new HashMap<String, ArrayList<RelatedConcept>>();
+        try{
+        Statement stmt = (Statement) con.createStatement();
+
+        con.setAutoCommit(false);
+        String modDocUri=doc.getUri();
+        if(modDocUri.contains("'")){
+            modDocUri=modDocUri.replace("'", "''");
+        }
+        String modDocName=doc.getName();
+        if(modDocName.contains("'")){
+            modDocName=modDocName.replace("'", "''");
+        }
+        String docInsertingQuery = "INSERT INTO documents(docURI,name,size) VALUES (\'"+modDocUri+"\',\'"+modDocName+"\',"+doc.getSize()+")";
+        stmt.executeUpdate(docInsertingQuery);
+        String getDocId = "SELECT * from documents WHERE docURI=\'"+modDocUri+"\'";
+        ResultSet rs1= stmt.executeQuery(getDocId);
+        int docId = 0;
+        while (rs1.next()) {
+            docId= rs1.getInt("docId");
+        }
+
+        PreparedStatement insertConcept = null;
+        PreparedStatement insertConceptDoc = null;
+//        con.setAutoCommit(false);
+        String insertConceptQuery ="INSERT INTO concepts(conceptName) VALUES (?)";
+        String insertConceptDocQuery ="INSERT INTO concept_doc(conceptId,docId,frequency,titleStrength,importance,strength) VALUES (?,?,?,?,?,?)";
+        insertConcept = con.prepareStatement(insertConceptQuery);
+        insertConceptDoc= con.prepareStatement(insertConceptDocQuery);
+        for (String e : concept.keySet()) {
+
+            String getConceptQuery = "SELECT conceptId from concepts WHERE conceptName=\'"+e.replaceAll("'", "''")+"\'";
+            ResultSet rsCon= stmt.executeQuery(getConceptQuery);
+            int existConceptId=0;
+            while (rsCon.next()) {
+                existConceptId= rsCon.getInt("conceptId");
+            }
+            int temp = 0;
+            
+            if(existConceptId==0){
+                insertConcept.setString(1, e);                
+                insertConcept.executeUpdate();
+                con.commit();
+
+                String getConId ="SELECT LAST_INSERT_ID()";
+                ResultSet rs2= stmt.executeQuery(getConId);
+                while (rs2.next()) {
+                    temp= rs2.getInt(1);
+                }
+            }else{
+                temp=existConceptId;
+            }
+
+            insertConceptDoc.setInt(1, temp);
+            insertConceptDoc.setInt(2, docId);
+            insertConceptDoc.setInt(3, concept.get(e).getFreequency());
+            insertConceptDoc.setInt(4, concept.get(e).getTitleStrength());
+            insertConceptDoc.setFloat(5, concept.get(e).getImportance());
+            insertConceptDoc.setInt(6, concept.get(e).getStrength());
+            insertConceptDoc.executeUpdate();
+            con.commit();
+        }
+
+
+        String getconceptQuery = "SELECT concepts.conceptName, concepts.conceptId FROM concepts JOIN concept_doc ON concepts.conceptId= concept_doc.conceptId WHERE concept_doc.docId="+docId;
+        ResultSet rs= stmt.executeQuery(getconceptQuery);
+        HashMap<String,Integer> conceptMap = new HashMap<String, Integer>();
+
+         while (rs.next()) {
+                String conName = rs.getString("conceptName");
+                int conId=rs.getInt("conceptId");
+                conceptMap.put(conName, conId);
+          } //end while
+
+        PreparedStatement insertRelDoc = null;
+        String insertConceptRelDocQuery ="INSERT INTO relationships(type,frequency,is_aStrength, part_ofStrength, strength,conceptId,isHead) VALUES "+"(?,?,?,?,?,?,?)";
+        insertRelDoc = con.prepareStatement(insertConceptRelDocQuery);
+
+        PreparedStatement insertRelConDoc = null;
+        String insertRelConDocQuery ="INSERT INTO related_concept(conceptId,relId) VALUES "+"(?,?)";
+        insertRelConDoc = con.prepareStatement(insertRelConDocQuery);
+        for (String e : concept.keySet()) {
+            relationships=concept.get(e).getRelationships();
+            if(!relationships.isEmpty()){
+                for (String f : relationships.keySet()) {
+                    if(relationships.get(f)!=null){
+                        for(RelatedConcept g:relationships.get(f)){
+                            insertRelDoc.setString(1, g.getType());
+                            insertRelDoc.setInt(2, g.getFreequency());
+                            insertRelDoc.setInt(3, g.getIsStrength());
+                            insertRelDoc.setInt(4, g.getPartStrength());
+                            insertRelDoc.setInt(5, g.getStrength());
+                            int conceptId=0;
+                            if(conceptMap.get(f)!=null){
+                                conceptId=conceptMap.get(f);
+                            }
+                            
+                            insertRelDoc.setInt(6,conceptId );
+                            insertRelDoc.setBoolean(7, g.isHead());
+                            insertRelDoc.executeUpdate();
+                            con.commit();
+
+                            String getConId ="SELECT LAST_INSERT_ID()";
+                            ResultSet rs2= stmt.executeQuery(getConId);
+                            int temp1 = 0;
+                            while (rs2.next()) {
+                                temp1= rs2.getInt(1);
+                            }
+
+                            int oriCon=0;
+                            if(conceptMap.get(e)!=null){
+                                oriCon=conceptMap.get(e);
+                            }
+                            insertRelConDoc.setInt(1, oriCon);
+                            insertRelConDoc.setInt(2, temp1);
+                            insertRelConDoc.executeUpdate();
+                            con.commit();
+                        }
+                    }
+                }
+            }
+        }
+
+
+        }catch (Exception e) {
+            try {
+                con.rollback();
+            } catch (SQLException ex) {
+                Logger.getLogger(DBConnector.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            e.printStackTrace();
+        }finally{
+            try {
+                con.setAutoCommit(true);
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(DBConnector.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println("Disconnected from database");
+        }
+
+
+    }
+}
---
+++  sigmac/SCDBServer/src/smc/testMain.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package smc;
+
+import com.mysql.jdbc.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import server.DBConnector;
+import server.DBManager;
+import smc.Document;
+
+/**
+ *
+ * @author Thilina
+ */
+public class testMain {
+    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException{
+        DBConnector db1=new DBConnector();
+        DBManager dbm=new DBManager();
+        Connection conn=(Connection) db1.getConnection();
+
+//        try{
+//          Statement stmt = (Statement) conn.createStatement();
+//          String getconceptString = "SELECT * from concept";
+//        ResultSet rs= stmt.executeQuery(getconceptString);
+//        HashMap<String,Integer> conceptMap = new HashMap<String, Integer>();
+////        ResultSetMetaData rsmd = (ResultSetMetaData) rs.getMetaData();
+////          int columnsNumber = rsmd.getColumnCount();
+//
+//          while (rs.next()) {
+//                String conName = rs.getString("concept");
+//                int conId=rs.getInt("id");
+//                conceptMap.put(conName, conId);
+//          }
+//
+//                for (String e : conceptMap.keySet()) {
+//                    System.out.println(e+"   "+conceptMap.get(e));
+//                }
+//
+//          conn.close();
+//          System.out.println("Disconnected from database");
+//        }catch (Exception e) {
+//          e.printStackTrace();
+//        }
+
+//        Deserialize des=new Deserialize();
+//        Optimizer op=new Optimizer();
+//        dbm.updateDB(conn, doc);
+    }
+
+}
