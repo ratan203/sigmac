fcbfdfa748e89b90772aedddf0130d5f3049e4b1
jayaprabathfernando <jayaprabathfernando@gmail.com>
2012-08-27 13:50:14 +0000

    testing method added

---  sigmac/SMC/src/smc/Document.java
+++  sigmac/SMC/src/smc/Document.java
@@ -249,4 +249,69 @@ public class Document implements Serializable {
         }
         return map;
     }
+
+    public void changeConcept(String from, String to){
+        if(this.doc.containsKey(from)){ //wory only if old name is in the map
+            Concept fc=this.doc.remove(from);
+            fc.deleteRelationshipsToConcept(to);
+            if(this.doc.containsKey(to)){ // new name also in the map
+                Concept remove = this.doc.remove(to);
+                remove.setFreequency(fc.getFreequency()+remove.getFreequency());
+                remove.modifyImprotance(fc.getStrength());
+                remove.addRelatedConcepts(fc.getRelationships());
+                remove.deleteRelationshipsToConcept(to);
+                this.doc.put(to, remove);
+                //here the update concept object with new name is available
+            }else{
+                fc.setName(to);
+                this.doc.put(to, fc);
+            }
+            //concepts which are related to  old concept
+            HashMap<String, ArrayList<RelatedConcept>> relationships = fc.getRelationships();
+            Set<String> keySet = relationships.keySet();
+            for(String key : keySet){  //each rel in the old concept
+                Concept rConcept = this.doc.get(key);
+                if(rConcept==null){
+                    System.out.println("NULLLL :"+key);
+                    System.out.println("From : "+from);
+                    System.out.println("To : "+to);
+                }
+                ArrayList<RelatedConcept> remove = rConcept.getRelationships().remove(from);
+                if(rConcept.getRelationships().containsKey(to)){ // if relation has to new one
+                    for(RelatedConcept r : remove){
+                        rConcept.addRelatedConcept(r);   //modify the new one adding oldones relations
+                    }
+                }else{
+                   rConcept.getRelationships().put(to, remove);
+                }
+            }
+
+        }else{
+            return;
+        }
+    }
+
+    public void testDocForRelations(){
+        Set<String> keySet = this.doc.keySet();
+        for(String key : keySet){
+            if(!key.matches(".*[A-Za-z]+.*")){
+                System.out.println("Empty Concept found on the doc");
+            }
+            Concept con=this.doc.get(key);
+            HashMap<String, ArrayList<RelatedConcept>> relationships = con.getRelationships();
+            Set<String> keySet1 = relationships.keySet();
+            for(String key1 : keySet1){
+                if(this.doc.containsKey(key1)){
+
+                }else{
+                    System.out.println("Concept : "+key);
+                    System.out.println("Related Concept : "+ key1);
+                    System.out.println("relationship to none existing concept");
+                }
+                if(!key1.matches(".*[A-Za-z]+.*")){
+                    System.out.println("Empty Relation Found for concept : "+ key);
+                }
+            }
+        }
+    }
 }
